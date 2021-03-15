56eb98e4140bf6299daf65926ee32a5008311f35
Sergii <sergiikovtun.vn@gmail.com>
2021-03-15 11:36:48 +0000

    add news component

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,34 @@
      <div class="card">
        <h2>Новости {{now}}</h2>
      </div>
-     <div class="card" v-for="item in news" :key="item">
-       <h3>{{item}}</h3>
-       <button class="btn" @click="isOpen = !isOpen">Open</button>
-       <p v-if="isOpen">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, dolore?</p>
-     </div>
+     <app-news title="This is title 1"> </app-news>
+     <app-news
+     v-for="item in news" 
+     :key="item"
+     :title="item"> 
+
+     </app-news>
    </div>
 </template>
 
+
 <script>
+import AppNews from './components/AppNews';
+
 export default {
 data() {
- return {
-   now: new Date().toLocaleDateString(),
-   isOpen: false,
-   news: [
-     'Новость 1',
-     'Новость 2',
-     'Новость 3'
-   ]
- }
-}
+  return {
+    now: new Date().toLocaleDateString(),
+    news: [
+      'Новость 1',
+      'Новость 2',
+      'Новость 3'
+    ]
+  }
+},
+  components:{
+    'app-news' : AppNews
+  }
 }
 </script>
 
---
+++ src/components/AppNews.vue
@@ -0,0 +1,19 @@
+<template>
+         <div class="card">
+       <h3>{{item}}</h3>
+         <h3>{{title}}</h3>
+       <button class="btn" @click="isOpen = !isOpen">Open</button>
+       <p v-if="isOpen">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, dolore?</p>
+     </div>
+</template>
+
+<script>
+export default {
+props: ['title'],
+data() {
+ return {
+    isOpen: false
+ }
+}
+}
+</script>
