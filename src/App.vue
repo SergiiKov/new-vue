5366c4043a7e503b43f8b6992469002512571131
Sergii <sergiikovtun.vn@gmail.com>
2021-03-16 10:38:47 +0000

    button component

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,77 @@
 <template>
   <the-header />
-   <the-header></the-header>
-   <div class="container pt-1">
-     <div class="card">
-       <h2>Новости {{now}}</h2>
-       <span>Open {{ openRate }} // Read {{ readRate }}</span>
-     </div>
-     <app-news title="This is title 1"> </app-news>
-     <app-news
-     v-for="item in news" 
-     :key="item.id"
-     :title="item.title"
-     :id="item.id"
-     :is-open="item.isOpen"
-     @open-news="openNews"
-     @read-news='readNews'
-     :was-read="item.wasRead"
-     @unmark="unreadNews" > 
-     </app-news>
-   </div>
+  <the-header></the-header>
+  <div class="container pt-1">
+    <div class="card">
+      <h2>Новости {{ now }}</h2>
+      <span>Open {{ openRate }} // Read {{ readRate }}</span>
+    </div>
+    <app-news title="This is title 1"> </app-news>
+    <app-news
+      v-for="item in news"
+      :key="item.id"
+      :title="item.title"
+      :id="item.id"
+      :is-open="item.isOpen"
+      @open-news="openNews"
+      @read-news="readNews"
+      :was-read="item.wasRead"
+      @unmark="unreadNews"
+    >
+    </app-news>
+  </div>
 </template>
 
-
 <script>
-import AppNews from './components/AppNews';
+import AppNews from "./components/AppNews";
 
 export default {
-data() {
-  return {
-    now: new Date().toLocaleDateString(),
-    news: [
-      {
-        title:'Новость 1',
-        id: 1,
-        isOpen: false,
-        wasRead: false
-      },
-            {
-        title:'Новость 2',
-        id: 2,
-        isOpen: false,
-           wasRead: false
-      },
-            {
-        title:'Новость 3',
-        id: 3,
-        isOpen: false,
-           wasRead: false
-      }
-
-    ],
-    openRate: 0,
-    readRate: 0
-  }
-},
-methods:{
-  openNews(data) {
-    this.openRate++,
-    console.log(data)
+  data() {
+    return {
+      now: new Date().toLocaleDateString(),
+      news: [
+        {
+          title: "Новость 1",
+          id: 1,
+          isOpen: false,
+          wasRead: false,
+        },
+        {
+          title: "Новость 2",
+          id: 2,
+          isOpen: false,
+          wasRead: false,
+        },
+        {
+          title: "Новость 3",
+          id: 3,
+          isOpen: false,
+          wasRead: false,
+        },
+      ],
+      openRate: 0,
+      readRate: 0,
+    };
   },
-  readNews(id) {
-    const idx =this.news.findIndex(news => news.id === id)
-    this.news[idx].wasRead= true
-    this.readRate++
+  methods: {
+    openNews(data) {
+      this.openRate++, console.log(data);
+    },
+    readNews(id) {
+      const idx = this.news.findIndex((news) => news.id === id);
+      this.news[idx].wasRead = true;
+      this.readRate++;
+    },
+    unreadNews(id) {
+      const news = this.news.find((news) => news.id === id);
+      news.wasRead = false;
+      this.readRate--;
+    },
   },
-  unreadNews(id) {
-const news = this.news.find(news => news.id === id)
-news.wasRead = false
-this.readRate--
-  }
-},
-  components:{
-    'app-news' : AppNews
-  }
-}
+  components: {
+    "app-news": AppNews,
+  },
+};
 </script>
 
 <style>
--- src/components/AppNews.vue
+++ src/components/AppNews.vue
@@ -1,67 +1,75 @@
 <template>
-    <div class="card">
-       <h3>{{item}}</h3>
-        <h3>{{title}}</h3>
-       <button class="btn" @click="open">{{ isNewsOpen ? 'Closed' : 'Open'}}</button>
-       <button class="btn danger" v-if="wasRead" @click="unmark">Not read</button>
-      <div v-if="isNewsOpen"> 
-        <hr />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, dolore?</p>
-        <button v-if="!wasRead" class="btn primary" @click="mark">Read news</button>
-      </div> 
+  <div class="card">
+    <h3>{{ item }}</h3>
+    <h3>{{ title }}</h3>
+    <AppButton  @action="open" :text="isNewsOpen ? 'Closed' : 'Open'"></AppButton>
+    <AppButton :color="'danger'" v-if="wasRead" @action="unmark" text="Not read"></AppButton>
+    <div v-if="isNewsOpen">
+      <hr />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+        dolore?
+      </p>
+      <AppButton text="Read news" v-if="!wasRead" color="primary" @action="mark"></AppButton>
     </div>
+  </div>
 </template>
 
 <script>
+import AppButton from './AppButton';
+
 export default {
-// props: ['title'],
-props: {
+  // props: ['title'],
+  props: {
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     id: Number,
     // isOpen: Boolean
-    wasRead:Boolean
-},
-// emits: ['open-news'],
-emits:{
-  'open-news': null,
-  'read-news'(id) {
-if (id) {
-  return true 
-  }
-  console.warn('No parametrs id for read-news')
-  return false
+    wasRead: Boolean,
   },
-  unmark: null
-//   'open-news'(num) {
-// if (num) {
-//   return true
-// }
-// console.warn('no data in open news emit')
-// return false
-//   }
-},
-data() {
- return {
-    isNewsOpen: this.isOpen
- }
-},
-methods: {
-    open(){
-        this.isNewsOpen= !this.isNewsOpen
-         if (this.isNewsOpen) {
-          this.$emit('open-news')   
-         } 
+  // emits: ['open-news'],
+  emits: {
+    "open-news": null,
+    "read-news"(id) {
+      if (id) {
+        return true;
+      }
+      console.warn("No parametrs id for read-news");
+      return false;
+    },
+    unmark: null,
+    //   'open-news'(num) {
+    // if (num) {
+    //   return true
+    // }
+    // console.warn('no data in open news emit')
+    // return false
+    //   }
+  },
+  data() {
+    return {
+      isNewsOpen: this.isOpen,
+    };
+  },
+  methods: {
+    open() {
+      this.isNewsOpen = !this.isNewsOpen;
+      if (this.isNewsOpen) {
+        this.$emit("open-news");
+      }
     },
     mark() {
-      this.isNewsOpen = false
-      this.$emit('read-news', this.id)
+      this.isNewsOpen = false;
+      this.$emit("read-news", this.id);
     },
     unmark() {
-      this.$emit('unmark', this.id)
-    }
-}
-}
+      this.$emit("unmark", this.id);
+    },
+  },
+  components: {
+    AppButton
+  }
+};
 </script>
